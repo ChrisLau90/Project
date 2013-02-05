6cc5294b86b68fce8487530c0ea4129a1c0b2e13
ChrisLau90 <chrislau@hotmail.co.uk>
2013-02-05 16:39:08 +0000

    Animation updated
    now resets to standing when key is not being pressed

--- entities.js
+++ entities.js
@@ -10,40 +10,63 @@ var PlayerEntity = me.ObjectEntity.extend({
 		// set the default horizontal & vertical speed (accel vector)
 		this.setVelocity(3,15);
 
+        this.animationspeed = me.sys.fps / 3;
+
 		// set the display to follow the position on both axis
 		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 
         // set animations
-        this.addAnimation("standStep", [0,1]);              //standing + first step
+        this.addAnimation("stand", [0]); //standing
+        this.addAnimation("step", [1]); //first step
         this.addAnimation("run", [2,3,4,5,6,7,8,9,10,11]);  //running
 
-        this.setCurrentAnimation("standStep", function(){
-            this.setCurrentAnimation("run");
-        });
+        this.isMoving = false;
+
+        /*------
+         if(!this.isCurrentAnimation("stand")){
+            this.setCurrentAnimation("stand", function(){
+                this.setCurrentAnimation("step", function(){
+                    this.setCurrentAnimation("run");
+                });
+            });
+         }
+         */
+
 	},
 
 	/*----------------------------------
 		Constructor
 	-----------------------------------*/
 	update: function() {
+
         if (me.input.isKeyPressed('left')) {
             // flip the sprite on horizontal axis
             this.flipX(true);
             // update the entity velocity
             this.vel.x -= this.accel.x * me.timer.tick;
+            this.isMoving = true;
+
         } else if (me.input.isKeyPressed('right')) {
             // unflip the sprite
             this.flipX(false);
             // update the entity velocity
             this.vel.x += this.accel.x * me.timer.tick;
+            this.isMoving = true;
+
         } else {
             this.vel.x = 0;
-            // RESET THE ANIMATION(?)
-            if(!this.isCurrentAnimation("standStep")){
-                this.setCurrentAnimation("standStep", function(){
-                    this.setCurrentAnimation("run");
+            //this.setCurrentAnimation("stand");
+            if(!this.isCurrentAnimation("stand")){
+                this.setCurrentAnimation("stand", function(){
+                    if(this.isMoving){
+                        this.setCurrentAnimation("step", function(){
+                            this.setCurrentAnimation("run");
+                        });
+                    }
                 });
             }
+            this.isMoving = false;
+            this.setAnimationFrame();
         }
         if (me.input.isKeyPressed('jump')) {
             // make sure we are not already jumping or falling
@@ -60,14 +83,14 @@ var PlayerEntity = me.ObjectEntity.extend({
         this.updateMovement();
 
         // update animation if necessary
-        if (this.vel.x!=0 || this.vel.y!=0) {
-            // update objet animation
-            this.parent(this);
-            return true;
-        }
+        //if (this.vel.x!=0 || this.vel.y!=0) {
+            // update object animation
+        //}
+        this.parent(this);
+        return true;
 
         // else inform the engine we did not perform
         // any update (e.g. position, animation)
-        return false;
+        //return false;
     }
 });
