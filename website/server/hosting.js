069677a304709607a092fcf5cf2c7e5a419225c7
Chris Lau <Chrislau@hotmail.co.uk>
2013-04-08 16:04:50 +0000

    Servertings

--- website/server/hosting.js
+++ website/server/hosting.js
@@ -4,13 +4,20 @@ var path = require('path');
 
 app.configure(function(){
   app.use(express.static(path.join(__dirname, '../')))
+  app.use(express.bodyParser())
 });
 
 app.listen(8888);
 console.log('Listening on port 8888');
 
-val handler = function(req,res) {
+var handler = function(req,res) {
 	res.send("hello")
 }
 
+var postHandler = function(req, res) {
+	console.log(req.param('thing'))
+	res.send(200)
+}
+
 app.get("/foo",handler)
+app.post("/foo",postHandler)
